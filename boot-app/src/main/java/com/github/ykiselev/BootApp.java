f433766826ca9053de1460000c066716de3edceb
Uze <Uze@yandex.ru>
2017-10-07 00:53:29 +0300

    1

---
+++ boot-app/src/main/java/com/github/ykiselev/BootApp.java
@@ -0,0 +1,12 @@
+package com.github.ykiselev;
+
+/**
+ * @author Yuriy Kiselev (uze@yandex.ru).
+ */
+
+public final class BootApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ module/src/main/java/com/github/ykiselev/App.java
@@ -0,0 +1,11 @@
+package com.github.ykiselev;
+
+/**
+ * @author Yuriy Kiselev (uze@yandex.ru).
+ */
+public final class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
