30471c0483b4f71b2db0b5c6d6ada2be60383cf9
Uze <Uze@yandex.ru>
2017-10-07 01:26:27 +0300

    2

--- boot-app/src/main/java/com/github/ykiselev/BootApp.java
+++ boot-app/src/main/java/com/github/ykiselev/BootApp.java
@@ -1,12 +1,15 @@
 package com.github.ykiselev;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Yuriy Kiselev (uze@yandex.ru).
  */
-
+@SpringBootApplication
 public final class BootApp {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(BootApp.class, args);
     }
 }
